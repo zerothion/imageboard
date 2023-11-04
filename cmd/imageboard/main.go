d19b4edddeff376af7dbd9f74a1d67d00f8a5b00
zerothion <leonid.paukov@yandex.ru>
2023-11-05 01:30:50 +0200

    feat: added `.env` file loading to main

--- cmd/imageboard/main.go
+++ cmd/imageboard/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log/slog"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	if err := godotenv.Load(); err != nil {
+		slog.Warn("Failed to load .env file", "err", err)
+	}
 
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.21.0
 
 require (
 	github.com/google/uuid v1.3.1
+	github.com/joho/godotenv v1.5.1
 )
